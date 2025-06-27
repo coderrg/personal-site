366baf6939056ba91f688a76254cebe869720c51
Rhythm Garg <typhoonrg@gmail.com>
2025-06-26 22:43:08 -0700

    rm old

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,62 +26,6 @@ export default function Home() {
           />
         </div>
         <h1 className="text-black dark:text-white text-3xl grid place-items-center justify-start">Rhythm Garg 👋</h1>
-
-        <h2 className="header-text mt-8">Currently</h2>
-        <div className="mt-8 content-column">
-          <ul>
-            <li>
-              OpenAI
-            </li>
-            <li>
-              Stanford University (on leave)
-            </li>
-          </ul>
-        </div>
-
-        <h2 className="header-text mt-4">Previously</h2>
-        <div className="mt-4 content-column">
-          <ul>
-            <li>
-              Google
-            </li>
-            <li>
-              Bridgewater Associates
-            </li>
-            <li>
-              Actively.ai
-            </li>
-            <li>
-              TreeHacks
-            </li>
-            <li>
-              Facebook
-            </li>
-            <li>
-              Stanford AI Lab
-            </li>
-          </ul>
-        </div>
-
-        <h2 className="header-text mt-4">Projects</h2>
-        <div className="mt-4 content-column">
-          <ul>
-            Co-created with my good friend <a href="https://miles.land/" className="underline">Miles</a>:
-            <li className="project-bullet">
-              AI meeting notes app called <a href="https://www.paxo.ai/" className="underline">Paxo</a> that
-              we grew to $20k in ARR. We didn&apos;t spend any money on advertising, and
-              we ended up selling the business!
-            </li>
-            <li className="project-bullet">
-              Semantic observability tool called Watchpost to give businesses a way
-              to monitor the quality of their generative AI model outputs.
-            </li>
-            <li className="project-bullet">
-              End-to-end encrypted location sharing app for family safety called <a href="https://heylatitude.com/" className="underline">Latitude</a>.
-            </li>
-          </ul>
-        </div>
-
         <h2 className="header-text mt-4">Contact</h2>
         <div className="mt-4 content-column">
           <ul>
